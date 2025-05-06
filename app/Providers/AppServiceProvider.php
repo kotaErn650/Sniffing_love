5d424233de800ab51d7d4f8c5ef76e2d017ad1f3
Jose Hernando Lopez <kota012011@gmail.com>
2025-05-06 15:07:18 -0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Evitar que Faker se cargue en producción si no está instalado
+        if ($this->app->environment('production') && !class_exists(\Faker\Factory::class)) {
+            $this->app->bind(\Faker\Generator::class, function () {
+                return new \App\Helpers\DummyDataGenerator; // Crea una clase alternativa si es necesario
+            });
+        }
     }
 
     /**
