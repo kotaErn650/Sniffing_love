943cb8a550eeabd1cc1bb65f7d4e38d12ebd613e
joseLopez <kota012011@gmail.com>
2025-05-16 10:02:12 -0500

    funcionalidad completa Veterinarias

--- resources/views/veterinarias/index.blade.php
+++ resources/views/veterinarias/index.blade.php
@@ -8,8 +8,8 @@
     <div class="py-8 bg-black" style="background-image: url('/img/cielo.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat;">
         <div class="container">
             <div class="d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center mb-3 gap-2">
-                @if (Auth::user()->id_rol ==1)
-                <a href="{{ route('veterinarias.create') }}" class="btn btn-info">➕Nueva Veterinaria</a>
+                @if (Auth::user()->id_rol == 1)
+                    <a href="{{ route('veterinarias.create') }}" class="btn btn-info">➕ Nueva Veterinaria</a>
                 @endif
             </div>
 
@@ -17,19 +17,19 @@
                 <table class="table table-bordered table-hover align-middle text-nowrap">
                     <thead class="table-dark">
                         <tr>
-                            <th>Click en el Enlace!!👇</th>
-                            <th scope="col">ID</th>
-                            <th scope="col">Nombre</th>
-                            <th scope="col">NIT</th>
-                            <th scope="col">Dirección</th>
-                            <th scope="col">Teléfono</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Horario</th>
-                            <th scope="col">Activa</th>
-                            <th scope="col">Administrador</th>
-                            <th scope="col">Calificación</th>
-                            <th scope="col">Fecha Registro</th>
-                            <th scope="col">Acciones</th>
+                            <th>Enlace</th>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th class="d-none d-sm-table-cell">NIT</th>
+                            <th class="d-none d-sm-table-cell">Dirección</th>
+                            <th class="d-none d-md-table-cell">Teléfono</th>
+                            <th class="d-none d-md-table-cell">Email</th>
+                            <th class="d-none d-md-table-cell">Horario</th>
+                            <th>Activa</th>
+                            <th class="d-none d-lg-table-cell">Administrador</th>
+                            <th class="d-none d-lg-table-cell">Calificación</th>
+                            <th class="d-none d-lg-table-cell">Fecha Registro</th>
+                            <th>Acciones</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -38,12 +38,14 @@
                                 'https://www.nexdu.com/co/cali-vac/empresa/animal-center-veterinaria-eps-cali-470233',
                                 'https://comunidadamoranimal.com/turnos/',
                                 'https://huellitasfelices.com/adoptanos/',
+                                'https://veterinariavillacolombia.com/',
                             ];
 
                             $imagenes = [
                                 '/img/vete_center.jpg',
                                 '/img/vete_amorAnimal.png',
                                 '/img/vete_huellasFelices.jpg',
+                                '/img/vete_villacol.jpg'
                             ];        
                         @endphp
 
@@ -54,29 +56,29 @@
                                         <img src="{{ $imagenes[$loop->index] ?? '/img/default.jpg' }}" 
                                              alt="Veterinaria" 
                                              class="img-fluid rounded"
-                                             style="max-width: 80px; height: auto; object-fit: cover;">
+                                             style="max-width: 80px; height: auto;">
                                     </a>
                                 </td>
                                 <td>{{ $v->id_veterinaria }}</td>
                                 <td>{{ $v->nombre }}</td>
-                                <td>{{ $v->nit ?? 'N/A' }}</td>
-                                <td>{{ $v->direccion }}</td>
-                                <td>{{ $v->telefono }}</td>
-                                <td>{{ $v->email }}</td>
-                                <td>{{ $v->horario_apertura }} - {{ $v->horario_cierre }}</td>
+                                <td class="d-none d-sm-table-cell">{{ $v->nit ?? 'N/A' }}</td>
+                                <td class="d-none d-sm-table-cell">{{ $v->direccion }}</td>
+                                <td class="d-none d-md-table-cell">{{ $v->telefono }}</td>
+                                <td class="d-none d-md-table-cell">{{ $v->email }}</td>
+                                <td class="d-none d-md-table-cell">{{ $v->horario_apertura }} - {{ $v->horario_cierre }}</td>
                                 <td>
                                     <span class="badge {{ $v->activa ? 'bg-success' : 'bg-secondary' }}">
                                         {{ $v->activa ? 'Sí' : 'No' }}
                                     </span>
                                 </td>
-                                <td>{{ $v->usuarioAdmin->name ?? 'No asignado' }}</td>
-                                <td>{{ $v->calificacion_promedio ?? 'N/A' }}</td>
-                                <td>{{ $v->fecha_registro ? $v->fecha_registro->format('Y-m-d H:i') : 'No disponible' }}</td>
+                                <td class="d-none d-lg-table-cell">{{ $v->usuarioAdmin->name ?? 'No asignado' }}</td>
+                                <td class="d-none d-lg-table-cell">{{ $v->calificacion_promedio ?? 'N/A' }}</td>
+                                <td class="d-none d-lg-table-cell">{{ $v->fecha_registro ? $v->fecha_registro->format('Y-m-d H:i') : 'No disponible' }}</td>
                                 <td>
                                     <div class="d-flex flex-column flex-md-row gap-1">
                                         <a href="{{ route('veterinarias.show', $v->id_veterinaria) }}" class="btn btn-sm btn-primary">Ver</a>
-                                         @if (Auth::user()->id_rol ==1)
-                                        <a href="{{ route('veterinarias.edit', $v->id_veterinaria) }}" class="btn btn-sm btn-warning">Editar</a>
+                                        @if (Auth::user()->id_rol == 1)
+                                            <a href="{{ route('veterinarias.edit', $v->id_veterinaria) }}" class="btn btn-sm btn-warning">Editar</a>
                                         @endif
                                     </div>
                                 </td>
@@ -90,5 +92,5 @@
                 </table>
             </div>
         </div>
-    </div>
+    </div> 
 </x-app-layout>
